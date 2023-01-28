124e5f674dfa5ccf0abfa94388627ce85a017375
Agustí Pou Company <agustipcom@gmail.com>
2023-01-28 11:56:18 +0100

    Add heroku

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ app.use((request, response) => {
   response.status(404).json({ error: 'Not Found' })
 })
 
-const PORT = 3001
+// Using Heroku PORT
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
